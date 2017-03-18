c63551c4c928cad67e9b37c35c8054c8ba86548c
Minghe Huang <h.minghe@gmail.com>
2017-03-18 16:16:59 +0800

    query finished

--- server/functions/query/index.js
+++ server/functions/query/index.js
@@ -1,19 +1,21 @@
 var AWS = require('aws-sdk');
 var dynamodb = new AWS.DynamoDB();
 
-var params = {
-  ExpressionAttributeValues: {
-    ":uri_value": {
-      S: "https://minghe.me"
+function buildQuery(opt) {
+  return {
+    ExpressionAttributeValues: {
+      ":uri_value": {
+        S: opt.uri,
+      },
     },
-  },
-  KeyConditionExpression: "uri = :uri_value",
-  // ProjectionExpression: "id",
-  TableName: "Comments"
-};
+    KeyConditionExpression: "uri = :uri_value",
+    // ProjectionExpression: "id",
+    TableName: "Comments"
+  };
+}
 
 exports.handle = function(e, ctx, cb) {
-  dynamodb.query(params, function(err, data) {
+  dynamodb.query(buildQuery(e), function(err, data) {
     if (err) {
       return ctx.fail('query error: ' + err)
     }
