7925b1afb3082ffc6de8840412ab285562dcc007
Minghe Huang <h.minghe@gmail.com>
2017-03-22 11:44:03 +0800

    setup develop infra

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Editor, EditorState } from 'draft-js'
+
+import styles from './styles.css'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    }
+  }
+
+  change(editorState) {
+    this.setState({ editorState })
+  }
+
+  submit() {
+    const { editorState } = this.state
+    const content = editorState.getCurrentContent()
+  }
   render() {
     return (
-      <div>
-        hello
+      <div className={ styles.container }>
+        <Editor
+          editorState={ this.state.editorState }
+          onChange={ ::this.change }
+        />
+        <button onClick={ ::this.submit }> Submit </button>
       </div>
     )
   }
