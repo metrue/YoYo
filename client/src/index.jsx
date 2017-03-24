24682f3ae9405bf2e737c1093ab6e820434bcec1
Minghe Huang <h.minghe@gmail.com>
2017-03-24 16:29:53 +0800

    done prototype

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,33 +1,51 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Editor, EditorState } from 'draft-js'
 
-import styles from './styles.css'
+import api from './api'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      editorState: EditorState.createEmpty(),
+      value: '',
     }
   }
 
-  change(editorState) {
-    this.setState({ editorState })
+  change(e) {
+    const value = e.target.value
+    this.setState({
+      value,
+    })
   }
 
   submit() {
-    const { editorState } = this.state
-    const content = editorState.getCurrentContent()
+    const { value } = this.state
+    api.submit({
+      user: 'test-user-1',
+      date: (new Date()).toISOString(),
+      uri: window.location.href,
+      content: value,
+    }).then((res) => {
+      if (res.status === 200) {
+        const { errorMessage } = res.data
+        if (errorMessage) {
+          console.warn(errorMessage)
+        } else {
+          console.warn(res.data)
+        }
+      }
+      console.warn(`${res.status} - ${res.statusText}`)
+      return new Error(`${res.statusText}`)
+    }).catch((e) => {
+      console.warn(e)
+    })
   }
+
   render() {
     return (
-      <div className={ styles.container }>
-        <Editor
-          editorState={ this.state.editorState }
-          onChange={ ::this.change }
-        />
+      <div>
+        <textarea onChange={ ::this.change } />
         <button onClick={ ::this.submit }> Submit </button>
       </div>
     )
