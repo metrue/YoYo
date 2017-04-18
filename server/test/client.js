d17832ef39db41287f93b3746bf68e73421be120
Minghe Huang <h.minghe@gmail.com>
2017-04-18 18:04:08 +0800

    clean up

--- server/test/client.js
+++
@@ -1,8 +0,0 @@
-const clientFactory = require('aws-api-gateway-client')
-const config = require('./config.json')
-
-module.exports = clientFactory.newClient({
-  region: config.region,
-  invokeUrl: config.invoke_url,
-  apiKey: config.apiKey
-})
--- server/test/index.js
+++
@@ -1,5 +0,0 @@
-const client = require('./client')
-const create = require('./create')
-const get = require('./get')
-
-create(client, get)
