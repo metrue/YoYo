f89d15011f519485219ad1ffbfa7550759438b13
Minghe Huang <h.minghe@gmail.com>
2017-04-25 14:41:24 +0800

    enable YoYo close and open

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -6,11 +6,114 @@ import api from './api'
 import styles from './styles.css'
 import { maybeEmailAddress } from './utils'
 
+const {
+  array,
+  string,
+  func,
+} = React.PropTypes
+
+const OpenIcon = ({ onClick }) => (
+  <div onClick={ onClick } className={ styles.YoYoOpenIcon }>
+    <svg width="28" height="24" className="result-item-expando">
+      <g transform="translate(14 12)">
+        <g className="expando-glyph">
+          <polygon points="-1.7,-5 3.3,0 -1.7,5 -2.9,3.8 1,0 -2.9,-3.8"></polygon>
+        </g>
+      </g>
+    </svg>
+    <span className={ styles.YoYoLabelTitle }> 想说点什么? </span>
+  </div>
+)
+
+OpenIcon.propTypes = {
+  onClick: func,
+}
+
+const CloseIcon = ({ onClick }) => (
+  <div onClick={ onClick } className={ styles.YoYoCloseIcon }>
+    <svg width="28" height="24" className="result-item-expando expanded">
+      <g transform="translate(14 12)">
+        <g className="expando-glyph">
+          <polygon points="-1.7,-5 3.3,0 -1.7,5 -2.9,3.8 1,0 -2.9,-3.8"></polygon>
+        </g>
+      </g>
+    </svg>
+    <span className={ styles.YoYoLabelTitle }> 收起评论 </span>
+  </div>
+)
+
+CloseIcon.propTypes = {
+  onClick: func,
+}
+
+const YoYoCommentList = ({ list }) => (
+  <div className={ styles.YoYoComentListContainer }>
+    {
+      list.map(c => (
+        <div className={ styles.YoYoComentItemContainer }>
+          <div className={ styles.YoYoComentItemUserAndDate }>
+            <div className={ styles.YoYoComentItemUser }>
+              { c.user }
+              <span className={ styles.YoYoComentItemDate }> - { moment(c.date).format('YYYY-MM-DD HH:MM') } </span>
+            </div>
+          </div>
+          <div className={ styles.YoYoComentItemText }>
+            <p>
+              { c.text }
+            </p>
+          </div>
+          <div>
+            <div className={ styles.YoYoCommentItemBottomBorder } />
+          </div>
+        </div>
+      ))
+    }
+  </div>
+)
+
+YoYoCommentList.propTypes = {
+  list: array,
+}
+
+const YoYoCommentBox = ({ text, onCommentTextChange, onEmailChange, onPublish }) => (
+  <div className={ styles.YoYoBoxContainer }>
+    <div className={ styles.YoYoInputArea }>
+      <textarea
+        className={ styles.YoYoComentTextArea }
+        value={ text }
+        onChange={ onCommentTextChange }
+      />
+    </div>
+    <div className={ styles.YoYoUserAction }>
+      <input
+        className={ styles.YoYoEmailInput }
+        type="text"
+        placeholder="leave email to get updates"
+        onChange={ onEmailChange }
+      />
+      <button
+        className={ styles.YoYoCommentPublishButton }
+        onClick={ onPublish }
+      >
+        Publish
+      </button>
+    </div>
+  </div>
+)
+
+YoYoCommentBox.propTypes = {
+  text: string,
+  onCommentTextChange: func,
+  onEmailChange: func,
+  onPublish: func,
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      open: false,
       text: '',
       email: '',
       list: [],
@@ -92,58 +195,38 @@ class App extends React.Component {
     }
   }
 
+  openYoYo() {
+    this.setState({ open: true })
+  }
+
+  closeYoYo() {
+    this.setState({ open: false })
+  }
+
   render() {
     const {
       list,
       text,
+      open,
     } = this.state
+
+    if (open) {
+      return (
+        <div className={ styles.YoYoContainer }>
+          <CloseIcon onClick={ ::this.closeYoYo } />
+          <YoYoCommentBox
+            text={ text }
+            onCommentTextChange={ ::this.commentTextChange }
+            onEmailChange={ ::this.commentEmailChange }
+            onPublish={ ::this.publish }
+          />
+          <YoYoCommentList list={ list } />
+        </div>
+      )
+    }
     return (
       <div className={ styles.YoYoContainer }>
-        <div className={ styles.YoYoBoxContainer }>
-          <div className={ styles.YoYoInputArea }>
-            <textarea
-              className={ styles.YoYoComentTextArea }
-              value={ text }
-              onChange={ ::this.commentTextChange }
-            />
-          </div>
-          <div className={ styles.YoYoUserAction }>
-            <input
-              className={ styles.YoYoEmailInput }
-              type="text"
-              placeholder="leave email to get updates"
-              onChange={ ::this.commentEmailChange }
-            />
-            <button
-              className={ styles.YoYoCommentPublishButton }
-              onClick={ ::this.publish }
-            >
-              Publish
-            </button>
-          </div>
-        </div>
-        <div className={ styles.YoYoComentListContainer }>
-          {
-            list.map(c => (
-              <div className={ styles.YoYoComentItemContainer }>
-                <div className={ styles.YoYoComentItemUserAndDate }>
-                  <div className={ styles.YoYoComentItemUser }>
-                    { c.user }
-                    <span className={ styles.YoYoComentItemDate }> - { moment(c.date).format('YYYY-MM-DD HH:MM') } </span>
-                  </div>
-                </div>
-                <div className={ styles.YoYoComentItemText }>
-                  <p>
-                    { c.text }
-                  </p>
-                </div>
-                <div>
-                  <div className={ styles.YoYoCommentItemBottomBorder } />
-                </div>
-              </div>
-            ))
-          }
-        </div>
+        <OpenIcon onClick={ ::this.openYoYo } />
       </div>
     )
   }
