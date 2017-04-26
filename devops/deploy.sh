80024569819a178bfeda0b8cc237ced5ffa56c48
Minghe Huang <h.minghe@gmail.com>
2017-04-26 17:38:33 +0800

    seperate steps

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -11,10 +11,11 @@ END
 rsync -avz -e 'ssh' backend root@45.33.106.101:/tmp/YoYo/
 rsync -avz -e 'ssh' devops root@45.33.106.101:/tmp/YoYo/
 ssh ${target_host} <<END
-  cd /tmp/YoYo
+  rm -rf /tmp/YoYo
+
+  git clone https://github.com/metrue/YoYo /tmp/YoYo
 
   docker rm \$(docker stop \$(docker ps -a -q --filter ancestor=yoyo  --format="{{.ID}}"))
-
 
   docker run --rm -p 80:80 -p 443:443 \
     -v /etc/letsencrypt:/etc/letsencrypt \
