e3adb9599a4c49a0cd632633c362f461c5bec99c
Minghe Huang <h.minghe@gmail.com>
2017-04-30 15:02:26 +0800

    fix the async/await cause creation delay issue: need to think more

--- backend/src/server/dal/comments.js
+++ backend/src/server/dal/comments.js
@@ -5,6 +5,8 @@ import { withMailer } from './utils'
 @withMailer
 export default class Comments extends BaseDal {
   async create(obj) {
+    await super.create(obj)
+
     const { parent, text, uri } = obj
     // eslint-disable-next-line
     const item = await this.findOne({ _id: ObjectID(parent) })
@@ -12,7 +14,5 @@ export default class Comments extends BaseDal {
       const appedUriText = `${text} - ${uri}`
       await this.mailer.send(item.user, appedUriText)
     }
-
-    return super.create(obj)
   }
 }
