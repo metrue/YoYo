b9533502dd65f3514a99ea2726b62b40255605c9
Minghe Huang <h.minghe@gmail.com>
2017-05-17 23:53:06 +0800

    use new domain

--- devops/letsencrypt.sh
+++ devops/letsencrypt.sh
@@ -4,7 +4,29 @@ docker run --rm -p 80:80 -p 443:443 \
   -v /etc/letsencrypt:/etc/letsencrypt \
   quay.io/letsencrypt/letsencrypt auth \
   --standalone -m h.minghe@gmail.com --agree-tos \
-  -d yoyo.minghe.me
+  -d api.yiqie.me
+
+docker run --rm -p 80:80 -p 443:443 \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  quay.io/letsencrypt/letsencrypt renew \
+  --standalone
+
+docker run --rm -p 80:80 -p 443:443 \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  quay.io/letsencrypt/letsencrypt auth \
+  --standalone -m h.minghe@gmail.com --agree-tos \
+  -d admin.yiqie.me
+
+docker run --rm -p 80:80 -p 443:443 \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  quay.io/letsencrypt/letsencrypt renew \
+  --standalone
+
+docker run --rm -p 80:80 -p 443:443 \
+  -v /etc/letsencrypt:/etc/letsencrypt \
+  quay.io/letsencrypt/letsencrypt auth \
+  --standalone -m h.minghe@gmail.com --agree-tos \
+  -d client.yiqie.me
 
 docker run --rm -p 80:80 -p 443:443 \
   -v /etc/letsencrypt:/etc/letsencrypt \
