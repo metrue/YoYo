5379285ee6ba27d98408cac8f38e1864fdbd97f3
Minghe Huang <h.minghe@gmail.com>
2017-05-18 00:10:28 +0800

    fix typo

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
-export const API_HOST = 'https://apyiqie.me/v1/api'
+export const API_HOST = 'https://api.yiqie.me/v1/api'
 // export const API_HOST = 'http://45.33.106.101/v1/api'
 // export const API_HOST = 'https://yoyo.minghe.me/v1/api'
 // export const DOMAIN = 'https://minghe.me'
