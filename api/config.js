efbafea59bbf8bacdac88d7847cc5fb029aa0dbc
Minghe Huang <h.minghe@gmail.com>
2018-06-21 12:04:37 +0800

    revert

--- api/config.js
+++ api/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  YOYO_EMAIL: 'a@a.com',
-  YOYO_DB_TABLE: 'YoYo-testing',
-  SITE_OWNER_EMAIL: 'mod@mod.com',
+  YOYO_EMAIL: process.env.YOYO_EMAIL,
+  YOYO_DB_TABLE: process.env.YOYO_DB_TABLE,
+  SITE_OWNER_EMAIL: process.env.SITE_OWNER_EMAIL,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
 }
